52499b2a44f27e40e343975880bbd5c1003f1a99
Fourteen98 <emuhiydeen6@gmail.com>
2022-07-22 15:17:30 +0000

    made changes to utitlities functionn

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import './style.css';
-import { addScore, refreshScores } from './modules/utilities.js';
+import { addScore, getScores } from './modules/utilities.js';
 
 const form = document.getElementById('form');
 const refresh = document.getElementById('refresh');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  addScore(e);
+  addScore(e).catch((err) => {
+      console.log(err);
+    });
   form.reset();
 });
 
 refresh.addEventListener('click', () => {
-  refreshScores();
+  getScores().catch((err) => {
+    console.log(err);
+  });
 });
 
-window.addEventListener('load', refreshScores);
+window.addEventListener('load', getScores);
--- src/modules/utilities.js
+++ src/modules/utilities.js
@@ -1,34 +1,41 @@
-export const refreshScores = async () => {
+export const getScores = async () => {
   const scoreBoard = document.querySelector('.score-board');
   scoreBoard.innerHTML = '';
-  const response = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/U89UpEn5EXu1MQahYhm3/scores/',
-  );
-  const json = await response.json();
+  try{
+    const response = await fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/U89UpEn5EXu1MQahYhm3/scores/',
+    );
+    const json = await response.json();
 
-  json.result.forEach((score) => {
-    scoreBoard.innerHTML += `
+    json.result.forEach((score) => {
+      scoreBoard.innerHTML += `
     <div class="score"><span>${score.user} :</span><span>${score.score}</span></div>
     `;
-  });
+    });
+  } catch(err){
+    console.log(err);
+  }
+
 };
 
 export const addScore = async (e) => {
-  const response = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/U89UpEn5EXu1MQahYhm3/scores/',
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        name: 'My cool new game',
-        user: `${e.target.name.value}`,
-        score: `${e.target.score.value}`,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+  try{
+    return await fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/U89UpEn5EXu1MQahYhm3/scores/',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          user: `${e.target.name.value}`,
+          score: `${e.target.score.value}`,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
       },
-    },
-  );
-  return response;
+    );
+  } catch (err){
+    console.log(err);
+  }
 };
 
-window.addEventListener('load', refreshScores);
+window.addEventListener('load', getScores);
